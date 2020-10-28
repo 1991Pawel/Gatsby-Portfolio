f2df71d0252cedef0c09ede6d88c74c37231bdfb
Pawel L <virtuozoo@o2.pl>
2020-10-28 12:53:28 +0100

    cleanup

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Navigation = () => {
+    <nav>
+        <ul>
+            <li>
+                <a href="">test</a>
+            </li>
+        </ul>
+    </nav>
+} 
