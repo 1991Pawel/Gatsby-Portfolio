f72dd8ee105d2ba88a16eb366124643b8019356d
Pawel L <virtuozoo@o2.pl>
2020-10-30 00:17:24 +0100

    change case

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Navigation from './Navigation'
+import styled from 'styled-components'
+
+
+const StyledHeader = styled.header`
+  grid-column:1 /-1;
+  padding:0 1rem;
+`;
+
+const HeaderWrapper = styled.div`
+  max-width:1200px;
+  margin:0 auto;
+`;
+
+
+const Header = () => (
+  <StyledHeader>
+    <HeaderWrapper>
+      <Navigation />
+    </HeaderWrapper>
+  </StyledHeader>
+)
+
+export default Header
