82f5076dbef4c21a344266a0b08df53e9910265e
Pawel L <virtuozoo@o2.pl>
2020-10-30 14:58:17 +0100

    change logo size

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components';
 
 const StyledLogo = styled.div`
-    font-size:2rem;
-    font-weight:bold;
+    font-size:2.5rem;
+    font-weight:700;
 `;
 
 const Logo = () => <StyledLogo>PAVDEV</StyledLogo>
